aeebd72cf8d37817743153ad135b49ba233cf643
mayjunjuly0313 <tank6138@gmail.com>
2021-05-02 22:07:55 +0900

    first commit

---
+++ src/personal/jdbc/orm/Main.java
@@ -0,0 +1,16 @@
+package personal.jdbc.orm;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+
+public class Main {
+	private static final Scanner in = new Scanner(System.in);
+	private static final PrintStream out = System.out;
+
+	public static void main(String[] args) {
+		out.println("Done");
+	}
+}
+
+
